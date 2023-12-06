899c0558e768fe04df5e17d5a7fd66e7d745fec9
Felipe Santos <felipe.santos@ws-solution.com>
2023-12-05 21:33:26 -0300

    Na pasta Models na classe Atividades, alterei o tipo do atributo DataCriacao de String para DateTime

--- API_Gerenciamento_Atividades/Models/Atividades.cs
+++ API_Gerenciamento_Atividades/Models/Atividades.cs
@@ -2,10 +2,11 @@
 {
     public class Atividades
     {
+        // cada um desses atributos equivale uma coluna no banco de dados
         public int ID { get; set; }
         public string Titulo { get; set; }
         public string Descricao { get; set; }
-        public string DataCriacao { get; set; }
+        public DateTime DataCriacao { get; set; }
         public string Status { get; set; }
     }
 }
