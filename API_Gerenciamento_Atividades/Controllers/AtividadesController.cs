94b49c26f78563a8958ac9a854c4c96f0cdee443
Felipe Santos <felipesantossf@outlook.com>
2023-12-10 07:36:33 -0300

    Adicionando comentários

--- API_Gerenciamento_Atividades/Controllers/AtividadesController.cs
+++ API_Gerenciamento_Atividades/Controllers/AtividadesController.cs
@@ -28,12 +28,12 @@ namespace API_Gerenciamento_Atividades.Controllers
         {
             try
             {
-                if (_context.Atividades == null)
+                if (_context.Atividades == null) // verificar se há valores válidos no banco de dados
                 {
-                    return NotFound();
+                    return NotFound(); // retorna erro caso não seja encontrado nada 
                 }
 
-                return await _context.Atividades.ToListAsync();
+                return await _context.Atividades.ToListAsync(); // se houver informações, é retornado as informações
             }
             catch (Exception ex)
             {
@@ -47,14 +47,14 @@ namespace API_Gerenciamento_Atividades.Controllers
         {
             try
             {
-                var atividade = await _context.Atividades.FindAsync(ID);
+                var atividade = await _context.Atividades.FindAsync(ID); // criado a variável atividade para verificar se o ID fornecido existe no banco de dados, se existir será armazenado na variavel atividade
 
-                if (atividade == null)
+                if (atividade == null) // verifica se o valor existe no banco de dados
                 {
-                    return StatusCode(400, $"ERRO! ID {ID} não localizado");
+                    return StatusCode(400, $"ERRO! ID {ID} não localizado"); // caso o ID informado não exista é retornado a mensagem de erro
                 }
 
-                return atividade;
+                return atividade; // se o ID existir é retornado a variavel atividade com todas as informações 
             }
             catch (Exception ex)
             {
@@ -67,37 +67,38 @@ namespace API_Gerenciamento_Atividades.Controllers
         {
             try
             {
-                if (atividades.ID != 0)
+                if (atividades.ID != 0) // verifica se o valor fornecido é diferente de zero, se o valor fornecido for diferente de zero é feito uma atualização em uma atividade e não uma criação de uma nova atividade
                 {
-                    var atividadeExiste = await _context.Atividades.FindAsync(atividades.ID);
+                    var atividadeExiste = await _context.Atividades.FindAsync(atividades.ID); // armazenando o valor fornecido em uma variavel
 
-                    if (atividadeExiste != null)
+                    if (atividadeExiste != null) // Se a atividade existir, as informações são atualizadas conforme valores fornecido
                     {
                         atividadeExiste.Titulo = atividades.Titulo;
                         atividadeExiste.Descricao = atividades.Descricao;
                         atividadeExiste.Status = atividades.Status;
 
-                        await _context.SaveChangesAsync();
+                        await _context.SaveChangesAsync(); // Salva as alterações no banco de dados
 
                         return Ok("Sucesso! ID atualizado com sucesso");
                     }
                     else
                     {
-                        return BadRequest("ERRO! ID não localizado");
+                        return BadRequest("ERRO! ID não localizado"); // Erro caso o ID fornecido não existe no banco de dados 
                     }
                 }
                 else
                 {
-                    if (await _context.Atividades.AnyAsync(a => a.Titulo == atividades.Titulo && a.Descricao == atividades.Descricao))
+                    if (await _context.Atividades.AnyAsync(a => a.Titulo == atividades.Titulo && a.Descricao == atividades.Descricao)) // Verifica se já existe uma atividade com o mesmo título e descrição
                     {
                         return BadRequest("ERRO! ID já cadastrado com este título e descrição");
                     }
                     else
-                    { 
-                        _context.Atividades.Add(atividades);
+                    {
+                        // Se o ID for enviado como zero, adiciona a nova atividade ao contexto do banco de dados
+                        _context.Atividades.Add(atividades); 
                         await _context.SaveChangesAsync();
 
-                        return CreatedAtAction(nameof(getAtividadeByID), new { id = atividades.ID }, atividades);
+                        return CreatedAtAction(nameof(getAtividadeByID), new { id = atividades.ID }, atividades); // Retorna uma resposta com os detalhes da nova atividade 
                     }
                 }       
             } 
