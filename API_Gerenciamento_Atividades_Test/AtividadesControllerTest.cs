78a869a64bf8d1941f8f6cc4eac09043056d869b
Felipe Santos <felipesantossf@outlook.com>
2023-12-13 19:19:24 -0300

    Criação do projeto para teste unitario - fonte: https://www.youtube.com/watch?v=9odWJcPG9DE&list=PLWNaqtzH6CWRUd0iIEJotRrpwtXXnFROE&index=5

---
+++ API_Gerenciamento_Atividades_Test/AtividadesControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace API_Gerenciamento_Atividades_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
